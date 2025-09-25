91862055111a4dc6a5356d8bda879765dda72e89
Simon Lebedev <sam_moshenko@mail.ru>
2025-09-25 17:28:08 +0500

    Initial Commit

---
+++ NeighboursNear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeighboursNear
+//
+//  Created by Simon on 25.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NeighboursNear/NeighboursNearApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeighboursNearApp.swift
+//  NeighboursNear
+//
+//  Created by Simon on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct NeighboursNearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
